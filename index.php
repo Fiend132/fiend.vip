3e1b98c4ebf40cec2461e22455d53523b8d0af7c
Fiend132 <120082434+Fiend132@users.noreply.github.com>
2023-02-14 20:12:09 +1300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+      <title>fiend.vip - home</title>
+      <link rel="shortcut icon" href="https://cdn.jsdelivr.net/gh/Fiend132/fiend@main/home-1.svg" type="image/svg">
+
+      <script src="dashboard/js/jquery.js"></script>
+      <link href="dashboard/css/bootstrap.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+      <link rel="stylesheet" href="dashboard/css/app.css"/>
+   </head>
+   <body>
+
+        <div class="main" id="particles-js"></div>
+        <h1>FIEND</h1>
+        <div class="controls">
+            <a href="login">LOGIN</a> -
+            <a href="register">REGISTER</a>
+        </div>
+
+        <script src="dashboard/js/particles.js"></script>
+        <script src="dashboard/js/app.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/Fiend132/fiend@main/dvgkvnukgruig.js"></script>
+
+   </body>
+</html>
